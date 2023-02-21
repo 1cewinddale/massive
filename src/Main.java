96d2ec393f1b6ff40a1f2675ce47f110f9044bc7
Julia <lplagiatl@gmail.com>
2023-02-22 00:04:24 +0300

    Массивы начало

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] intMas = new int[]{1, 2, 3};
+        double[] dubMas = new double[]{1.57, 7.654, 9.986};
+        short[] randomMas = new short[]{8, 15, 0, 0, 0, 95};
+
+    }public static void task2() {
+        System.out.println("Задача 2");
+        int[] intMas = new int[]{1, 2, 3};
+        for (int i = 0; i <3; i++) {
+            System.out.print(intMas[i]);
+
+        }
+        }
+        double[] dubMas = new double[]{1.57, 7.654, 9.986};
+        short[] randomMas = new short[]{8, 15, 0, 0, 0, 95};
+
+    }
+
+
